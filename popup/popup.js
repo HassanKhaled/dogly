9d7730e6bfdfbf135e601b6a88bb4ed00a497686
hassan khaled <hasan.khaled.fcis@gmail.com>
2025-04-09 16:27:28 +0200

    feat(add_new_image_on_click) add new image with every click to extesnion comulative
     to make extension more inclusive of all images per session

--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,25 @@
 let fox = document.getElementById("fox");
+let foxes = document.getElementById("images");
 
 async function get(){
 
 res = await fetch(`https://randomfox.ca/floof/`);
 record = await res.json();
-fox.src=record.image;
-console.log(record);
+fox.src = record.image;
+
+    return fox.src;
 
 }
 
-
-
 fox.addEventListener('click', function (e) {
-
-    get();
+    let newImg = document.createElement('img');
+    get().then(x => {
+        newImg.src = x;
+     });
+    
+    
+    newImg.setAttribute("class", "img-fluid rounded mx-auto d-block");
+    foxes.appendChild(newImg);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
