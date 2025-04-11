1e92a15c24504917d8ee0e2145cc31aab380a5bf
hassan khaled <hasan.khaled.fcis@gmail.com>
2025-04-11 19:02:08 +0200

    feat(add_button_to_add_new_images) add button which on click add new images to images container
     make the application much more focused and logically

--- popup/popup.js
+++ popup/popup.js
@@ -5,21 +5,28 @@ async function get(){
 
 res = await fetch(`https://randomfox.ca/floof/`);
 record = await res.json();
-fox.src = record.image;
-
-    return fox.src;
-
-}
-
-fox.addEventListener('click', function (e) {
+//fox.src = record.image;
     let newImg = document.createElement('img');
-    get().then(x => {
-        newImg.src = x;
-     });
+    
+    newImg.src = record.image;
     
     
     newImg.setAttribute("class", "img-thumbnail rounded my-2");
     foxes.prepend(newImg);
+    
+
+}
+
+fox.addEventListener('click', function (e) {
+get();
+    // let newImg = document.createElement('img');
+    // get().then(x => {
+    //     newImg.src = x;
+    //  });
+    
+    
+    // newImg.setAttribute("class", "img-thumbnail rounded my-2");
+    // foxes.prepend(newImg);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
