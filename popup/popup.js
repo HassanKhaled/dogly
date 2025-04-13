8a167a91f7064b9ed579664286a99f88ccc26e08
hassan khaled <hasan.khaled.fcis@gmail.com>
2025-04-13 18:09:43 +0200

    chore(remove_old_get_function_code) remove commeted code from popup.js code
     improve usablitiy and enhance future maintainablitiy

--- popup/popup.js
+++ popup/popup.js
@@ -17,21 +17,6 @@ setInterval(()=>{
 }, 2000);
 
 
-// async function get(){
-
-// res = await fetch(`https://randomfox.ca/floof/`);
-// record = await res.json();
-
-//     let newImg = document.createElement('img');
-    
-//     newImg.src = record.image;
-    
-//     newImg.setAttribute("class", " img-thumbnail rou mx-auto d-block my-2");
-//     foxes.prepend(newImg);
-    
-
-// }
-
 button.addEventListener('click', function (e) {
 get(foxes);
 
