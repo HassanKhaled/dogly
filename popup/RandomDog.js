25d4ce2198f815d4fa3da2a9af8a38b845ea6adb
hassan khaled <hasan.khaled.fcis@gmail.com>
2025-05-02 19:22:04 +0300

    fix(subbreed_misnaming_method_parameter) change and moify to match teh paramters missing b in subreed
     make teh function work again and enable further developrmeent

--- popup/RandomDog.js
+++ popup/RandomDog.js
@@ -38,7 +38,7 @@ export function clearOptions(selectElement) {
 
 export async function listSubbreed(breed = 'hound',subbreed){
 
-    let res = await fetch(`https://dog.ceo/api/breed/${breed}/${subreed}/images`);
+    let res = await fetch(`https://dog.ceo/api/breed/${breed}/${subbreed}/images`);
     let record = await res.json();
 
 
