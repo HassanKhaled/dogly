2946e048d337aca4301031b7628a3e4fd41e6e4c
hassan khaled <hasan.khaled.fcis@gmail.com>
2025-05-11 19:04:20 +0300

    feat(add_subreed_js) add subreed js file for seperation of concerns
     enhance and improve the code usability and code improvement and Ui and Ux

---
+++ popup/subbreed.js
@@ -0,0 +1,43 @@
+import { getDog, listBreed, listSubbreed , clearOptions } from './RandomDog.js';
+
+let button = document.getElementById("generate");
+let images = document.getElementById("images");
+
+let loader = document.getElementById("loader");
+let core = document.getElementById("main");
+let breed = document.getElementById("breed");
+let subbreed = document.getElementById("subbreed");
+
+core.style.display = "none";
+
+
+setInterval(()=>{
+
+    core.style.display = "block";
+    loader.style.display="none";
+
+}, 2000);
+
+
+button.addEventListener('click', function (e) {
+button.disabled = true;
+    getDog(images);
+button.disabled = false;
+
+
+});
+
+
+// breed.addEventListener("change", ()=>{
+
+//     clearOptions(subbreed);
+//     listSubbreed(breed.value,subbreed)
+
+// });
+
+
+// addEventListener("DOMContentLoaded", (event) => { 
+
+//     listBreed(breed);
+//    // listSubbreed('hound',subbreed);
+// });
