12789a13a55bfd85c6bf07f3cd4cef3f7720f24f
HassanKhaled <hasan.khaled.fcis@gmail.com>
2025-05-22 17:44:26 +0300

    style(remove_unused_ui_elements): remove all ui elements from breeds html file for ease the usage of extension
     enhance and help improve the UI and UX and ease of use of the extension

--- popup/breeds.js
+++ popup/breeds.js
@@ -1,7 +1,6 @@
 import { getDog, listBreed, listSubbreed , clearOptions ,listAllBreedSubbreeds } from './RandomDog.js';
 
-let button = document.getElementById("generate");
-let images = document.getElementById("images");
+
 
 let loader = document.getElementById("loader");
 let core = document.getElementById("main");
@@ -22,7 +21,7 @@ setInterval(()=>{
 
 
 addEventListener("DOMContentLoaded", (event) => { 
-    
+
     listAllBreedSubbreeds(breeds);
 
 });
